5c06b722f36ee86e55d18496799f551bc8dee727
Johnathan Nicholas <jtnicholas2014@gmail.com>
2020-10-08 15:44:34 +0100

    Added .gitignore

---
+++ src/main/java/Program.java
@@ -0,0 +1,9 @@
+import StudentRegistration.Course;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Course course = new Course("Computer Science & IT");
+        System.out.println(course);
+    }
+}
